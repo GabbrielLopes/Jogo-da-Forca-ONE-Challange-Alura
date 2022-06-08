6904bf14131dd38dcd2a883ec27229ca341e4c7f
GabbLps <jhosegabriel95@gmail.com>
2022-06-08 20:04:42 -0300

    codigo refatorado

--- script/adiciona-palavra.js
+++ script/adiciona-palavra.js
@@ -1,38 +1,46 @@
 let temas = document.querySelector("#theme");
 
-let palavras = {
-    objetos: ["MOUSE", "TECLADO", "CADEIRA", "HEADSET", "VIOLAO"],
-    dicaObjetos: ["se usa no computador","se usa no computador","tem na cozinha","se usa para jogar","quem tem dó é?"],
-    frutas: ["LARANJA,","ACEROLA","BANANA","TOMATE","MELANCIA"],
-    dicaFrutas: ["é uma cor","coceira","macaco gosta","ketchup","magali"],
-    veiculos: ["MAREA","UNO","CORSA","SONATA","POLO"],
-    dicaVeiculos: ["carro bombastico","carro para toda obra","capotei o ?","hyundai","pega moço"],
-    random: ["TRUCO","BACALHAU","CAFÉ","ALHO","VACA","VAMPIRO"],
-    dicaRandom: ["pede 6","especie de peixe","tira sono","tempero","muuu","morcego"]
-}
-
 function verificaSeTemNumero(val){
-    return !isNaN(val)
+
+    for(var i = 0;i < val.length;i++){
+        if(!isNaN(val[i])){
+            temNumero = true;
+        } else {
+            temNumero = false;
+        }
+    }
+    return Boolean(temNumero);
   }
 
 let palavraArmazenada;
 let = palavraEncontrada = false;
 function validaPalavra(palavra){
-    if(palavras.objetos.includes(palavra) || palavras.objetos.includes(palavra.toLowerCase())){
-        palavraEncontrada = true;
-        warningWordAdded.className = 'aviso-palavra-repetida';
-        warningWordAdded.textContent = 'Erro, essa palavra já foi adicionada!';
-        inputNewWord.value="";
-        setTimeout(function(){  // setando tempo para proxima função, para dar tempo de ver o efeito da transição
-            warningWordAdded.className = 'aviso-palavra-adicionada-none'
-            warningWordAdded.textContent = "Palavra adicionada com sucesso!"
-        }, 3000);
-        return;
-    } else {
-        palavraEncontrada = false;
-    } 
+    for(let tema = 0;tema < palavras.length;tema++) {
+        for(let nome = 0;nome < palavras[tema].length;nome++) {
+            if(palavras[tema][nome].includes(palavraArmazenada)){
+                palavraEncontrada = true;
+                console.log("Palavra encontrada!");
+                avisaPalavraRepetida();
+                return;
+            } else {
+                console.log("Palavra disponível!");
+                palavraEncontrada = false;
+            } 
+        };
+    };
 }
 
+
+function avisaPalavraRepetida() {
+    warningWordAdded.className = 'aviso-palavra-adicionada-none'
+    warningWordAdded.className = 'aviso-palavra-repetida';
+    warningWordAdded.textContent = 'Erro, essa palavra já foi adicionada!';
+    inputNewWord.value="";
+    setTimeout(function(){  // setando tempo para proxima função, para dar tempo de ver o efeito da transição
+        warningWordAdded.className = 'aviso-palavra-adicionada-none'
+        warningWordAdded.textContent = "Palavra adicionada com sucesso!"
+    }, 3000);
+}
 
 function avisaPalavraAdicionada() {
     warningWordAdded.className = 'aviso-palavra-adicionada'
@@ -53,23 +61,29 @@ function addWord() {
     }
     
     if(temas.value == 'objetos'){
-        palavras.objetos.push(palavraArmazenada);
-        palavras.dicaObjetos.push(inputHint.value);
+        objetos = 0
+        frutas = 1
+        veiculos = 2
+        aleatorio = 3
+        palavras[objetos].push(palavraArmazenada);
+        dicas[objetos].push(inputHint.value);
     } else if(temas.value == 'frutas'){
-        palavras.frutas.push(palavraArmazenada);
-        palavras.dicaFrutas.push(inputHint.value);
+        palavras[frutas].push(palavraArmazenada);
+        dicas[frutas].push(inputHint.value);
     } else if(temas.value == 'veiculo'){
-        palavras.veiculos.push(palavraArmazenada);
-        palavras.dicaVeiculos.push(inputHint.value);
+        palavras[veiculos].push(palavraArmazenada);
+        dicas[veiculos].push(inputHint.value);
     }else if(temas.value == 'random'){
-        palavras.random.push(palavraArmazenada);
-        palavras.dicaRandom.push(inputHint.value);
+        palavras[aleatorio].push(palavraArmazenada);
+        dicas[aleatorio].push(inputHint.value);
     }
 
     avisaPalavraAdicionada();
     inputNewWord.value="";
+    inputHint.value="";
     
     console.log(palavras);
+    
 }
 
 btnAddWord.addEventListener('click', addWord);
